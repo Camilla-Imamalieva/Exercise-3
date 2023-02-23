a6fa8ea254ea78028c8f86060aa2ceeb4085ed94
Camillar <camillacrew@mail.ru>
2023-02-23 16:35:34 +0400

    Добавили задачу Exercise_3

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int [] array = new int [n];
+double d = 0;
+for (int i = 0; i < n; i++)
+{
+    array [i] = i + 1;
+    d = Math.Pow(array[i], 3);
+    Console.WriteLine(d);
+} 
